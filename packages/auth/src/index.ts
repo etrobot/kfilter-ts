fa0260e95e3610ee2a0efaa4734e432a5cc46e4d
Frank Lin <etrobot@outlook.com>
2026-01-10 10:13:03 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@filter-ts/db";
+import * as schema from "@filter-ts/db/schema/auth";
+import { env } from "@filter-ts/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
